8b641196af00e42dc6cd654332f0e5a4590ac273
Ramadani <danisabaruddin98@gmail.com>
2025-09-06 16:08:09 +0800

    add option for add_order

--- customer/add_order.php
+++ customer/add_order.php
@@ -100,7 +100,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <h4>Items</h4>
             <div id="items">
                 <div class="row g-2 mb-2">
-                    <div class="col"><input type="text" name="item_type[]" class="form-control" placeholder="Item (SHIRT/TR...)" required></div>
+                    <select name="item_type[]" class="form-control item-type" required onchange="showWorkslip(this)">
+                        <option value="">Select Apparel</option>
+                        <option value="shirt">Shirt</option>
+                        <option value="trousers">Trousers</option>
+                        <option value="jacket">Jacket</option>
+                        <option value="baju_melayu">Baju Melayu</option>
+                    </select>
                     <div class="col"><input type="number" name="quantity[]" class="form-control" placeholder="Qty" required></div>
                     <div class="col"><input type="text" name="fabric_code[]" class="form-control" placeholder="Fabric Code"></div>
                     <div class="col"><input type="text" name="fabric_name[]" class="form-control" placeholder="Fabric Name"></div>
