8960c69beedd09263fbb1cb6cb78921a3268ea80
Ramadani <danisabaruddin98@gmail.com>
2025-09-07 16:16:59 +0800

    add filter

--- customer/overview.php
+++ customer/overview.php
@@ -10,37 +10,37 @@ $filter_apparel  = $_GET['apparel'] ?? '';
 $sql = "
     SELECT c.customer_id, c.customer_name, c.customer_phone,
            i.invoice_id, i.invoice_number,
-           'Shirt' AS apparel_type, s.item AS item, s.quantity AS qty, s.delivery_date
+           'SHIRT' AS apparel_type, s.item AS item, s.quantity AS qty, s.delivery_date
     FROM invoices i
     JOIN customers c ON i.customer_id = c.customer_id
-    JOIN shirt_workslip s ON i.invoice_id = s.invoice_id
+    JOIN workslip_shirts s ON i.invoice_id = s.invoice_id
 
     UNION ALL
 
     SELECT c.customer_id, c.customer_name, c.customer_phone,
            i.invoice_id, i.invoice_number,
-           'Trousers' AS apparel_type, t.item, t.quantity, t.delivery_date
+           'TROUSER' AS apparel_type, t.item, t.quantity, t.delivery_date
     FROM invoices i
     JOIN customers c ON i.customer_id = c.customer_id
-    JOIN trousers_workslip t ON i.invoice_id = t.invoice_id
+    JOIN workslip_trousers t ON i.invoice_id = t.invoice_id
 
     UNION ALL
 
     SELECT c.customer_id, c.customer_name, c.customer_phone,
            i.invoice_id, i.invoice_number,
-           'Jacket' AS apparel_type, j.item, j.quantity, j.delivery_date
+           'JACKET' AS apparel_type, j.item, j.quantity, j.delivery_date
     FROM invoices i
     JOIN customers c ON i.customer_id = c.customer_id
-    JOIN jacket_workslip j ON i.invoice_id = j.invoice_id
+    JOIN workslip_jacket j ON i.invoice_id = j.invoice_id
 
     UNION ALL
 
     SELECT c.customer_id, c.customer_name, c.customer_phone,
            i.invoice_id, i.invoice_number,
-           'Baju Melayu' AS apparel_type, b.item, b.quantity, b.delivery_date
+           'BAJU MELAYU' AS apparel_type, b.item, b.quantity, b.delivery_date
     FROM invoices i
     JOIN customers c ON i.customer_id = c.customer_id
-    JOIN bajumelayu_workslip b ON i.invoice_id = b.invoice_id
+    JOIN workslip_baju_melayu b ON i.invoice_id = b.invoice_id
 ";
 
 // Wrap query so we can filter safely
