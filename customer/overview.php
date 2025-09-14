fea03980137e5a97315982110256ac267b983b67
Ramadani <danisabaruddin98@gmail.com>
2025-09-14 09:20:41 +0800

    add navbar in overview.php

--- customer/overview.php
+++ customer/overview.php
@@ -28,6 +28,32 @@ $result = $conn->query($query);
 <body class="p-4">
     <div class="container">
         <h2 class="mb-4">Invoice Dashboard</h2>
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">CMS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="overview.php">Overview</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="add_order.php">Add Order</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
         <table id="invoiceTable" class="table table-striped table-bordered">
             <thead>
                 <tr>
