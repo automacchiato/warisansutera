7f7f29855e1abfcfd1fb66a53e10607ef4f6cd47
Ramadani <danisabaruddin98@gmail.com>
2025-09-14 09:11:09 +0800

    add link in index.php

--- customer/index.php
+++ customer/index.php
@@ -4,11 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Customer</title>
 </head>
 
 <body>
-
+    <div>
+        <h1>Customer Database</h1>
+        <div>
+            <a class="btn btn-primary" href="overview.php" role="button">Overview</a>
+        </div>
+    </div>
 </body>
 
 </html>
