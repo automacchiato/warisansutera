bb0badd4b66dcf62a57278ca25349e1fd708d439
Ramadani <danisabaruddin98@gmail.com>
2025-10-03 16:11:52 +0800

    test invoice id

--- test/index.php
+++ test/index.php
@@ -41,7 +41,7 @@ JOIN customers c ON i.customer_id = c.customer_id");
                         <td><?= $row['order_date'] ?></td>
                         <td><?= $row['delivery_date'] ?></td>
                         <td>
-                            <a href="generate_pdf.php?id=<?= $row['invoice_id'] ?>" class="btn btn-danger btn-sm">Download PDF</a>
+                            <a href="generate_pdf.php?invoice_id=<?= $row['invoice_id'] ?>" class="btn btn-danger btn-sm">Download PDF</a>
                         </td>
                     </tr>
                 <?php } ?>
