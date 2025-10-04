d5be710b7877ea07b1aa6d5a8078d44e7432aef6
Ramadani <danisabaruddin98@gmail.com>
2025-10-04 18:05:42 +0800

    adjust fabric direction

--- test/workslip_pdf.php
+++ test/workslip_pdf.php
@@ -191,7 +191,7 @@ while ($row = $items->fetch_assoc()) {
             $pdf->SetFont('Arial', 'B', 12);
             $pdf->Cell(47.5, 8, "Fabric Direction", 1, "C");
             $pdf->SetFont('Arial', '', 12);
-            $pdf->Cell(47.5, 8, $invoice['fabric_direction'], 1);
+            $pdf->Cell(47.5, 8, $work['fabric_direction'], 1);
 
             $pdf->Ln(5);
 
