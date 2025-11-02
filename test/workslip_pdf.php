6a5dadd41a6f82c62e91aa705fcbbef2e7a200f2
Ramadani <danisabaruddin98@gmail.com>
2025-11-02 16:02:45 +0800

    adjust

--- test/workslip_pdf.php
+++ test/workslip_pdf.php
@@ -787,7 +787,7 @@ while ($row = $items->fetch_assoc()) {
             $pdf->Cell(35, 10, $work['cleaning_type'], 1, 1, "C");
 
             //Drawing
-            $pdf->Image(__DIR__ . "/../customer/uploads/drawings/" . $work['drawing'], 110, 90, 90, 90);
+            $pdf->Image(__DIR__ . "/../customer/uploads/drawings/" . $work['drawing'], 110, 80, 90, 90);
 
             // Extra notes / signatures
             $pdf->Cell(0, 8, "Special Instructions: " . ($work['special_instructions'] ?? ""), 0, 1);
